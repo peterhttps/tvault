a963294dfda940d164bdbce2080e2d0ca5ab3db5
Pedro Lucas <plrf100@gmail.com>
2022-02-20 21:46:48 -0300

    fix(realm): removing realm

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
 import Navigation from './navigation/Navigation';
-import RealmContext from './database/index';
-
-const {RealmProvider} = RealmContext
 
 export default function App() {
   return (
-    <RealmProvider>
-      <Navigation />
-    </RealmProvider>
+    <Navigation />
   );
 }
 
